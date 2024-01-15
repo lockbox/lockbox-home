31bfb485c02ec92c1a9c5ce9fd625bc8f182de48
lockbox <lockbox.06@protonmail.com>
2024-01-15 04:04:25 -0500

    Update rust installation management

--- modules/rust.nix
+++ modules/rust.nix
@@ -2,9 +2,11 @@
 
 {
   home = {
+    # NOTE: let rustup manaage rust installations instead of nix,
+    # so that is why rustup is not added to packages, but we still
+    # add the cargo bin to the path
     packages = with pkgs; [
-      rustup
-      gcc
+      #rustup
     ];
     sessionPath = [ "$HOME/.cargo/bin" ];
   };
