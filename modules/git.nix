8961f03294ce2d35514cf515244da36efb5eb4cc
lockbox <lockbox@struct.foo>
2024-04-27 20:07:47 -0400

    add git configs and update x setup

--- modules/git.nix
+++ modules/git.nix
@@ -30,6 +30,34 @@
     extraConfig = {
       init.defaultBranch = "main";
       core.editor = "vim";
+
+      # update refs by default
+      rebase.updateRefs = true;
+      rebase.ff = "only";
+
+      # put branches in columns if there's room
+      column.ui = "auto";
+
+      # reuse recorded resolution
+      # "on rebase remember how i fixed merge conflict"
+      rerere.enabled = true;
+
+      # stash everything
+      alias.astash = "stash --all";
+
+      # safer version of push --force-with-lease, which is a safer form of
+      # push --force
+      alias.fpush = "push --force-if-includes";
+    };
+
+    # use lfs
+    lfs = {
+      enable = true;
+    };
+
+    # difftastic diff viewer
+    difftastic = {
+      enable = true;
     };
   };
 }
