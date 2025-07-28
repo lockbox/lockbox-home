113fb4650938a24834766131fb3ee94fa5e1ea19
Jordan Moore <lockbox@struct.foo>
2025-07-28 16:51:16 -0400

    feat: update wezterm config

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -2,8 +2,16 @@ local wezterm = require 'wezterm'
 
 local config = {}
 
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+--
 config.enable_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
+config.enable_wayland = true
 
 return config
 
